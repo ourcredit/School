65fe08f65bafd674e6887de548c0351054cdcd7d
zhaohejing2017 <zhaohejing@zhj125.cn>
2018-04-10 16:12:02 +0800

    www

--- dashboard/src/libs/SignalRAspNetCoreHelper.js
+++ dashboard/src/libs/SignalRAspNetCoreHelper.js
@@ -1,10 +1,10 @@
 import AppConsts from './appconst'
-class SignalRAspNetCoreHelper{
-    initSignalR(){
+class SignalRAspNetCoreHelper {
+    initSignalR() {
         var encryptedAuthToken = abp.utils.getCookieValue(AppConsts.authorization.encrptedAuthTokenName);
 
         abp.signalr = {
-            autoConnect: true,
+            autoConnect: false,
             connect: undefined,
             hubs: undefined,
             qs: AppConsts.authorization.encrptedAuthTokenName + "=" + encodeURIComponent(encryptedAuthToken),
