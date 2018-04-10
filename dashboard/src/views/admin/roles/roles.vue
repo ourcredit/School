14a1ed3861c2bad4ed22c0d5fe99713a5c1521a1
zhaohejing2017 <zhaohejing@zhj125.cn>
2018-04-10 10:20:51 +0800

    build success

--- dashboard/src/views/admin/roles/roles.vue
+++ dashboard/src/views/admin/roles/roles.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <Card>
-            <p slot="title">{{'Roles'|l}}</p>
+            <p slot="title">角色信息</p>
             <Dropdown slot="extra"  @on-click="handleClickActionsDropdown">
                 <a href="javascript:void(0)">
-                    {{'Actions'|l}}
+                   操作
                     <Icon type="android-more-vertical"></Icon>
                 </a>
                 <DropdownMenu slot="list">
@@ -67,151 +67,177 @@
 </template>
 <script>
 export default {
-    methods:{
-        create(){
-            this.editRole={isActive:true};
-            this.showModal=true;
-        },
-        async save(){
-            if(!!this.editRole.id){
-                this.$refs.roleForm.validate(async (val)=>{
-                    if(val){
-                        await this.$store.dispatch({
-                            type:'role/update',
-                            data:this.editRole
-                        })
-                        this.showEditModal=false;
-                        await this.getpage();
-                    }
-                })
-                
-            }else{
-                this.$refs.newRoleForm.validate(async (val)=>{
-                    if(val){
-                        await this.$store.dispatch({
-                            type:'role/create',
-                            data:this.editRole
-                        })
-                        this.showModal=false;
-                        await this.getpage();
-                    }
-                })
-            }
-            
-        },
-        pageChange(page){
-            this.$store.commit('role/setCurrentPage',page);
-            this.getpage();
-        },
-        pagesizeChange(pagesize){
-            this.$store.commit('role/setPageSize',pagesize);
-            this.getpage();
-        },
-        async getpage(){
+  methods: {
+    create() {
+      this.editRole = { isActive: true };
+      this.showModal = true;
+    },
+    async save() {
+      if (!!this.editRole.id) {
+        this.$refs.roleForm.validate(async val => {
+          if (val) {
             await this.$store.dispatch({
-                type:'role/getAll'
-            })
-        },
-        handleClickActionsDropdown(name){
-            if(name==='Create'){
-                this.create();
-            }else if(name==='Refresh'){
-                this.getpage();
-            }
-        }
-    },
-    data(){
-        return{
-            editRole:{},
-            showModal:false,
-            showEditModal:false,
-            newRoleRule:{
-                name:[{required:true,message:'Name is required',trigger: 'blur'}],
-                displayName:[{required:true,message:'DisplayName is required',trigger: 'blur'}],
-            },            
-            roleRule:{
-                name:[{required:true,message:'Name is required',trigger: 'blur'}],
-                displayName:[{required:true,message:'DisplayName is required',trigger: 'blur'}],
-            },
-            columns:[{
-                title:this.L('RoleName'),
-                key:'name'
-            },{
-                title:this.L('DisplayName'),
-                key:'displayName'
-            },{
-                title: this.L('Actions'),
-                key: 'action',
-                width:150,
-                render:(h,params)=>{
-                    return h('div',[
-                        h('Button',{
-                            props:{
-                                type:'primary',
-                                size:'small'
-                            },
-                            style:{
-                                marginRight:'5px'
-                            },
-                            on:{
-                                click:()=>{
-                                    this.editRole=this.roles[params.index];
-                                    this.showEditModal=true;
-                                }
-                            }
-                        },this.L('Edit')),
-                        h('Button',{
-                            props:{
-                                type:'error',
-                                size:'small'
-                            },
-                            on:{
-                                click:async()=>{
-                                    this.$Modal.confirm({
-                                        title:this.L(''),
-                                        content:this.L('Delete role'),
-                                        okText:this.L('Yes'),
-                                        cancelText:this.L('No'),
-                                        onOk:async()=>{
-                                            await this.$store.dispatch({
-                                                type:'role/delete',
-                                                data:this.roles[params.index]
-                                            })
-                                            await this.getpage();
-                                        }
-                                    })
-                                }
-                            }
-                        },this.L('Delete'))
-                    ])
-                }
-            }]
-        }
-    },
-    computed:{
-        roles(){
-            return this.$store.state.role.roles;
-        },
-        permissions(){
-            return this.$store.state.role.permissions;
-        },
-        totalCount(){
-            return this.$store.state.role.totalCount;
-        },
-        currentPage(){
-            return this.$store.state.role.currentPage;
-        },
-        pageSize(){
-            return this.$store.state.role.pageSize;
-        }
-    },
-    async created(){
-        this.getpage();
-        await this.$store.dispatch({
-            type:'role/getAllPermissions'
+              type: "role/update",
+              data: this.editRole
+            });
+            this.showEditModal = false;
+            await this.getpage();
+          }
         });
+      } else {
+        this.$refs.newRoleForm.validate(async val => {
+          if (val) {
+            await this.$store.dispatch({
+              type: "role/create",
+              data: this.editRole
+            });
+            this.showModal = false;
+            await this.getpage();
+          }
+        });
+      }
+    },
+    pageChange(page) {
+      this.$store.commit("role/setCurrentPage", page);
+      this.getpage();
+    },
+    pagesizeChange(pagesize) {
+      this.$store.commit("role/setPageSize", pagesize);
+      this.getpage();
+    },
+    async getpage() {
+      await this.$store.dispatch({
+        type: "role/getAll"
+      });
+    },
+    handleClickActionsDropdown(name) {
+      if (name === "Create") {
+        this.create();
+      } else if (name === "Refresh") {
+        this.getpage();
+      }
     }
-}
+  },
+  data() {
+    return {
+      editRole: {},
+      showModal: false,
+      showEditModal: false,
+      newRoleRule: {
+        name: [
+          { required: true, message: "Name is required", trigger: "blur" }
+        ],
+        displayName: [
+          {
+            required: true,
+            message: "DisplayName is required",
+            trigger: "blur"
+          }
+        ]
+      },
+      roleRule: {
+        name: [
+          { required: true, message: "Name is required", trigger: "blur" }
+        ],
+        displayName: [
+          {
+            required: true,
+            message: "DisplayName is required",
+            trigger: "blur"
+          }
+        ]
+      },
+      columns: [
+        {
+          title: "角色名",
+          key: "name"
+        },
+        {
+          title: "显示名",
+          key: "displayName"
+        },
+        {
+          title: "操作",
+          key: "action",
+          width: 150,
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      this.editRole = this.roles[params.index];
+                      this.showEditModal = true;
+                    }
+                  }
+                },
+                "编辑"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  on: {
+                    click: async () => {
+                      this.$Modal.confirm({
+                        title: this.L(""),
+                        content: this.L("Delete role"),
+                        okText: this.L("Yes"),
+                        cancelText: this.L("No"),
+                        onOk: async () => {
+                          await this.$store.dispatch({
+                            type: "role/delete",
+                            data: this.roles[params.index]
+                          });
+                          await this.getpage();
+                        }
+                      });
+                    }
+                  }
+                },
+                "删除"
+              )
+            ]);
+          }
+        }
+      ]
+    };
+  },
+  computed: {
+    roles() {
+      return this.$store.state.role.roles;
+    },
+    permissions() {
+      return this.$store.state.role.permissions;
+    },
+    totalCount() {
+      return this.$store.state.role.totalCount;
+    },
+    currentPage() {
+      return this.$store.state.role.currentPage;
+    },
+    pageSize() {
+      return this.$store.state.role.pageSize;
+    }
+  },
+  async created() {
+    this.getpage();
+    await this.$store.dispatch({
+      type: "role/getAllPermissions"
+    });
+  }
+};
 </script>
 
 
